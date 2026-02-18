6f039c4e74c7f7823948ec85f33ad6b0ed10adda
Nozanin Bahridinova <115372156+nbahridinova@users.noreply.github.com>
2026-02-18 16:45:08 -0500

    Update test_triangle.py

--- test_triangle.py
+++ test_triangle.py
@@ -1,24 +1,30 @@
 # test_triangle.py
-from triangle import classify_triangle
+import unittest
 import math
+from triangle import classify_triangle
 
-def test_equilateral():
-    assert classify_triangle(3, 3, 3) == "Equilateral"
+class TestTriangle(unittest.TestCase):
 
-def test_isosceles():
-    assert classify_triangle(5, 5, 8) == "Isosceles"
+    def test_equilateral(self):
+        self.assertEqual(classify_triangle(3, 3, 3), "Equilateral")
 
-def test_scalene():
-    assert classify_triangle(4, 5, 6) == "Scalene"
+    def test_isosceles(self):
+        self.assertEqual(classify_triangle(5, 5, 8), "Isosceles")
 
-def test_right_scalene():
-    assert classify_triangle(3, 4, 5) == "Scalene Right"
+    def test_scalene(self):
+        self.assertEqual(classify_triangle(4, 5, 6), "Scalene")
 
-def test_isosceles_right():
-    assert classify_triangle(1, 1, math.sqrt(2)) == "Isosceles Right"
+    def test_right_scalene(self):
+        self.assertEqual(classify_triangle(3, 4, 5), "Scalene Right")
 
-def test_not_a_triangle_zero():
-    assert classify_triangle(0, 1, 1) == "NotATriangle"
+    def test_isosceles_right(self):
+        self.assertEqual(classify_triangle(1, 1, math.sqrt(2)), "Isosceles Right")
 
-def test_not_a_triangle_inequality():
-    assert classify_triangle(1, 2, 3) == "NotATriangle"
+    def test_not_a_triangle_zero(self):
+        self.assertEqual(classify_triangle(0, 1, 1), "NotATriangle")
+
+    def test_not_a_triangle_inequality(self):
+        self.assertEqual(classify_triangle(1, 2, 3), "NotATriangle")
+
+if __name__ == "__main__":
+    unittest.main()
